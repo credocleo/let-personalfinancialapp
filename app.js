7b9dea8049209344d9b123c47b0783b4fd0b4e2b
credocleo <credocleo28@gmail.com>
2017-03-25 01:34:08 +0800

    Cleo First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCtwuSlpwRQtjGE84BEfTUFyOBrp3qY9lg",
+    authDomain: "let-personal-financial-app.firebaseapp.com",
+    databaseURL: "https://let-personal-financial-app.firebaseio.com",
+    storageBucket: "let-personal-financial-app.appspot.com",
+    messagingSenderId: "783424072158"
+  };
+  firebase.initializeApp(config);
